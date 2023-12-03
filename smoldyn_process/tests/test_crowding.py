90e1c29c9d1436f0be355d022fc0cee91ea871a9
alex patrie <alexanderpatrie@gmail.com>
2023-12-03 13:04:10 -0500

    adjustments

--- smoldyn_process/tests/test_crowding.py
+++ smoldyn_process/tests/test_crowding.py
@@ -4,15 +4,31 @@ from smoldyn import Simulation
 model_fp = 'smoldyn_process/models/model_files/minE_model.txt'
 
 sim = Simulation.fromFile(model_fp)
+
+species_names = [sim.getSpeciesName(index) for index in range(sim.count()['species'])]
+species_names.remove('empty')
+
+species_names = ['MinE']
+
 sim.run(1, 1)
 
-'''sim.addOutputData('molecules')
-sim.addCommand(cmd='listmols2 molecules', cmd_type='E')
-sim.run(sim.stop, sim.dt)
-data = sim.getOutputData('molecules')
+sim.addOutputData('molecules')
 
-print(data[0])'''
+for name in species_names:
+    dataname = f'{name}_data'
+    sim.addOutputData(dataname)
+    sim.addCommand(cmd=f'listmols3 {name} {dataname}', cmd_type='E')
 
-# print(data)
-# print(type(data))
+sim.run(1, 1)
+
+
+mol_data = {}
+for name in species_names:
+    dataname = f'{name}_data'
+    data = sim.getOutputData(dataname)
+    print(f'{name}: {data}')
+    #mol_data[dataname] = data
+
+
+print(mol_data)
 
