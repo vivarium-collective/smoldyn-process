e04a7887826f60365cce283a41bad686fb58fc3d
alex patrie <alexanderpatrie@gmail.com>
2023-12-03 19:34:44 -0500

    adjusted test scratch file

--- smoldyn_process/tests/test_crowding.py
+++ smoldyn_process/tests/test_crowding.py
@@ -1,4 +1,5 @@
 from smoldyn import Simulation
+import json
 
 
 model_fp = 'smoldyn_process/models/model_files/minE_model.txt'
@@ -10,25 +11,41 @@ species_names.remove('empty')
 
 species_names = ['MinE']
 
-sim.run(1, 1)
+#sim.addOutputData('molecules')
 
-sim.addOutputData('molecules')
+#sim.addOutputData('molpos')
 
-for name in species_names:
+'''for name in species_names:
     dataname = f'{name}_data'
     sim.addOutputData(dataname)
     sim.addCommand(cmd=f'listmols3 {name} {dataname}', cmd_type='E')
+'''
+
+
+for spec in species_names:
+    dataname = f'molPos_{spec}'
+    sim.addOutputData(dataname)
+    sim.addCommand(f'molpos {spec} {dataname}')
 
 sim.run(1, 1)
 
+pos_data = {}
+for spec in species_names:
+    dataname = f'molPos_{spec}'
+    data = sim.getOutputData(dataname)
+    pos_data[spec] = data
 
-mol_data = {}
+
+with open('outputs.json', 'w') as f:
+    json.dump(pos_data, f, indent=4)
+
+
+
+'''mol_data = {}
 for name in species_names:
     dataname = f'{name}_data'
     data = sim.getOutputData(dataname)
     print(f'{name}: {data}')
     #mol_data[dataname] = data
-
-
-print(mol_data)
+'''
 
